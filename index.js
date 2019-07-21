559c7db0cf4f0227f4e3eb4352984f84ba84ff3d
Arshad Kazmi <arshadkazmi42@gmail.com>
2019-07-21 23:26:25 +0530

    Got cookie read write using sqlite3 working

--- index.js
+++ index.js
@@ -1,45 +1,81 @@
-const sqlite3 = require('sqlite3').verbose();
-
-const BROWSERS = {
-  chrome: 'CHROME', 
-  firefox: 'FIREFOX'
-};
+const sqlite3 = require('sqlite3');
 
 
-const PATHS = {
-  [BROWSERS['chrome']]: '',
-  [BROWSERS['firefox']]: ''
-}
+function CookieCrud() {
 
-
-function CookieCrud(path) {
-
-  if (!path){
-    throw new Error('Invalid path');
-  }
-
-  this.db = this.connect(path)
-  this.browser = None
-}
-
-CookieCrud.prototype.setChrome = function() {
-  this.browser = BROWSERS['chrome'];
-}
-
-CookieCrud.prototype.setFirefox = function() {
-  this.browser = BROWSERS['firefox'];
+  this.db = null
 }
 
 CookieCrud.prototype.connect = function(path) {
-  return new sqlite3.Database(path, (err) => {
+  console.log(path);
+  this.db = new sqlite3.Database(path, (err) => {
     if (err) {
       console.error(err);
       throw new Error('Failed to connect to database');
     }
 
     console.info(`Connected to db ${path}`);
+  });
+
+  console.log(this.db);
+}
+
+
+// TODO Promisify this
+CookieCrud.prototype.readCookie = function(domain) {
+  const db = this.db;
+  db.serialize(function() {
+    db.all(`SELECT * from cookies WHERE host_key like '%${domain}%'`, function(err, row) {
+      if (err) {
+        console.error(err);
+        throw new Error('Failed to execute query');
+      }
+      
+      const { fields, values, placeholders } = parse(row[0])
+      const query = `INSERT INTO cookies ( ${fields.join(', ')} ) values ( ${placeholders.join(', ')} )`
+      const stmt = db.prepare(query, values);
+      stmt.run();
+      stmt.finalize();
+      return row
+    });
   })
 }
+
+
+CookieCrud.prototype.writeCookie = function(cookies) {
+  const db = this.db;
+  db.serialize(function () {
+    console.log(cookies);
+    for (cookie of cookies) {
+      const { fields, values } = parse(cookie)
+      const query = `INSERT INTO cookies ( ${fields.join(', ')} ) values ( ${values.join(', ')} )`
+      console.log(query);
+    }
+    // db.run()
+  })
+}
+
+const parse = (cookie) => {
+  const fields = [];
+  const values = [];
+  const placeholders = [];
+  for (let key in cookie) {
+    placeholders.push('?');
+    fields.push(key);
+    if (key === 'encrypted_value') {
+      values.push("'" + cookie[key].toString() + "'")
+    } else {
+      if (typeof cookie[key] === 'string') {
+        values.push("'" + cookie[key] + "'");
+      } else {
+        values.push(cookie[key])
+      }
+    }
+    
+  }
+
+  return { fields, values, placeholders };
+} 
 
 
 CookieCrud.prototype.read
--- tests/test.js
+++ tests/test.js
@@ -1,13 +1,20 @@
-const { expect } = require('chai');
+// const { expect } = require('chai');
 
-// Import your code which to be tested
+// // Import your code which to be tested
 
-// Initialize static data
+// // Initialize static data
 
-/**
-describe('description of the test', () => {
-  it('details of the specific test', () => {
-    expect(0).to.equal(0);
-  });
-});
-**/
+// /**
+// describe('description of the test', () => {
+//   it('details of the specific test', () => {
+//     expect(0).to.equal(0);
+//   });
+// });
+// **/
+
+const CookieCrud = require('../index');
+
+const Cookie = new CookieCrud();
+Cookie.connect('./Cookies');
+
+Cookie.readCookie('mail.google.com');
