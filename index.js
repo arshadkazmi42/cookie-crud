6e17889456e0f27e12e08ffd6c1954306445ca59
Arshad Kazmi <arshadkazmi42@gmail.com>
2019-07-21 14:07:45 +0530

    Initial class setup

--- index.js
+++ index.js
@@ -1,6 +1,48 @@
-const dummy = () => {
-    
+const sqlite3 = require('sqlite3').verbose();
+
+const BROWSERS = {
+  chrome: 'CHROME', 
+  firefox: 'FIREFOX'
+};
+
+
+const PATHS = {
+  [BROWSERS['chrome']]: '',
+  [BROWSERS['firefox']]: ''
 }
 
 
-module.exports = dummp;
+function CookieCrud(path) {
+
+  if (!path){
+    throw new Error('Invalid path');
+  }
+
+  this.db = this.connect(path)
+  this.browser = None
+}
+
+CookieCrud.prototype.setChrome = function() {
+  this.browser = BROWSERS['chrome'];
+}
+
+CookieCrud.prototype.setFirefox = function() {
+  this.browser = BROWSERS['firefox'];
+}
+
+CookieCrud.prototype.connect = function(path) {
+  return new sqlite3.Database(path, (err) => {
+    if (err) {
+      console.error(err);
+      throw new Error('Failed to connect to database');
+    }
+
+    console.info(`Connected to db ${path}`);
+  })
+}
+
+
+CookieCrud.prototype.read
+
+
+module.exports = CookieCrud;
