2d4d0f6718676c7e277769e001eebaa1ca5d5f0a
Arshad Kazmi <arshadkazmi42@gmail.com>
2019-08-27 08:14:48 +0530

    Chrome and Firefox support, Tests, Readme update

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
-const { Chrome, Firefox } = require('./lib');
+const Chrome = require('chrome-cookie');
+const Firefox = require('firefox-cookie');
 
 
-function CookieCrud(path) {
+function CookieCrud(chromePath, firefoxPath) {
 
-  this.chrome = new Chrome(path);
-  this.firefox = null;
+  this.chrome = new Chrome(chromePath);
+  this.firefox = new Firefox(firefoxPath);
 }
+
 
 CookieCrud.prototype.getChrome = function() {
   return this.chrome;
-}
+};
+
 
 CookieCrud.prototype.getFirefox = function() {
   return this.firefox;
-}
+};
 
 
 module.exports = CookieCrud;
